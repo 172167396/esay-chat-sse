56d590e6ae3337efc19333148b7e4b344c6b6a5d
dailu <172167396@qq.com>
2022-12-07 14:40:54 +0800

    [dailu][修改Dockerfile]

--- src/main/java/com/easychat/sse/config/MinIoClientConfig.java
+++ src/main/java/com/easychat/sse/config/MinIoClientConfig.java
@@ -1,12 +1,14 @@
 package com.easychat.sse.config;
 
 import io.minio.MinioClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @Configuration
 public class MinIoClientConfig {
 
@@ -20,6 +22,7 @@ public class MinIoClientConfig {
      */
     @Bean
     public MinioClient minioClient() {
+        log.info("minio url is:{},accessKey is: {},secretKey is: {}",minioProperties.getEndPoint(),minioProperties.getAccessKey(),minioProperties.getSecretKey());
         return MinioClient.builder()
                 .endpoint(minioProperties.getEndPoint())
                 .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
