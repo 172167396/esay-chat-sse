cf379507d721373f3b2c49b72709d2cd28f5248a
dailu <172167396@qq.com>
2024-08-05 16:40:22 +0800

    [dailu][增加logback]

--- src/main/java/com/easychat/sse/controller/CaptchaController.java
+++ src/main/java/com/easychat/sse/controller/CaptchaController.java
@@ -5,6 +5,7 @@ import com.wf.captcha.ArithmeticCaptcha;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,14 +18,17 @@ public class CaptchaController {
 
 
     @GetMapping("/captcha")
+    @ResponseBody
     public void captcha(HttpServletRequest request, HttpServletResponse response) {
         ArithmeticCaptcha captcha = new ArithmeticCaptcha(130, 48);
         captcha.setLen(2);
         captcha.getArithmeticString();
         String text = captcha.text();
         RedisUtil.set(request.getSession().getId(), text, 1800L);
+        response.setContentType("image/png");
         try (OutputStream out = response.getOutputStream()) {
             captcha.out(out);
+            log.info("验证码为:{}", text);
         } catch (IOException e) {
             try {
                 response.sendError(500);
